58f3b7d207910006be6bdc08a3e65027445f9fcc
Julian <julian-jordan.starks@fwcsstudents.org>
2024-09-09 11:51:25 -0400

    Add files via upload

---
+++ Starks_TIY_4_1.py
@@ -0,0 +1,8 @@
+pizzas = ["pepperoni", "meat lovers", "cheese"]
+for pizza in pizzas:
+    print(f"My favorite pizza is {pizza}.\n")
+
+print(f"{pizzas[0].title()} is my favorite pizza topping.")
+print(f"{pizzas[1].title()} is also good.")
+print(f"{pizzas[2].title()} is my least favorite type of pizza.")
+print("I really like pizza.")
