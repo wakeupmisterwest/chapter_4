814f14c98d3be4884b24487d176b465d775dd847
Julian <julian-jordan.starks@fwcsstudents.org>
2024-09-10 12:24:24 -0400

    Add files via upload

---
+++ dimensions.py
@@ -0,0 +1,9 @@
+dimensions = (200, 50)
+print("Original dimensions:")
+for dimension in dimensions:
+    print(dimension)
+
+dimension = (400, 100)
+print("\nModified dimensions:")
+for dimension in dimension:
+    print(dimension)
