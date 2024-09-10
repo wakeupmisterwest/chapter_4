3875ec8c8acd70ed878e8784af9b7422dcc58de9
Julian <julian-jordan.starks@fwcsstudents.org>
2024-09-10 12:48:21 -0400

    Add files via upload

---
+++ Starks_TIY_4_13.py
@@ -0,0 +1,10 @@
+menu = ("burger", "chicken", "steak", "mash potatoes", "mac and cheese")
+print("These foods are currently available:")
+for food in menu:
+    print(food)
+
+
+menu = ("burger", "chicken", "pork chop", "shrimp", "mac and cheese")
+print("\nThis is the updated menu:")
+for food in menu:
+    print(food)
